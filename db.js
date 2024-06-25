051bb01cdce7575dab6376d0fae1cd37ba0f1165
Dilanmadhu <dilanmadusanka405@gmail.com>
2024-06-26 00:31:27 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Dilan@0731',
+    password: '123',
     database: 'parcel'
 });
 
