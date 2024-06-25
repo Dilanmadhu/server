585e5f68858bc84d56562b96c5925cf5e0127a29
Dilan Madhusankha <dilanmadusanka5555@gmail.com>
2024-06-26 01:20:19 +0530

    test

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -10,7 +10,20 @@ app.get('/', (req, res) => {
 
 // Add your other routes here
 app.get('/indunil', (req, res) => {
-  res.json({ message: 'Hello Indunil' });
+    res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Simple HTML Page</title>
+    </head>
+    <body>
+      <h1>Hello, World!</h1>
+      <p>This is a simple HTML page rendered by Express.</p>
+    </body>
+    </html>
+  `);
 });
 
 module.exports.handler = serverless(app);
