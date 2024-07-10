679cd07c2dac126446f756e2c83cf1863f323f6e
Dilanmadhu <dilanmadusanka405@gmail.com>
2024-07-11 03:45:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const db = require('./db');
 const seprate = require('./Routes/userRoute');
-const serverless = require("serverless-http");
 
+// Middleware
+app.use(express.json());
 
-
+// Routes
 app.get('/a', (req, res) => {
-    res.send("successful")  
-})
+    res.send("successful");
+});
 
-app.use('/sep',seprate.router);
+app.use('/sep', seprate.router);
 
 app.get('/admin', (req, res) => {
     db.query('SELECT * FROM admin', (err, result) => {
-    if (err) {
-        console.error('Error executing query:', err);
-        res.status(500).send('Error executing query');
-        return;
-      }
-      res.json(result);})
-})
+        if (err) {
+            console.error('Error executing query:', err);
+            res.status(500).send('Error executing query');
+            return;
+        }
+        res.json(result);
+    });
+});
 
-
-app.listen(3000, ()=>{
-    console.log("server is listning")
-})
-
-module.exports.handler = serverless(app);
+// Start server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
