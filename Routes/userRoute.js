1e64686bc2addd754ac09d70b40c446e4360ebd1
Dilan Madusanka <dilanmadusanka405@gmail.com>
2025-12-31 08:30:39 +0530

    Update userRoute.js

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -5,4 +5,15 @@ router.use('/',(req, res, next) =>{
     res.send("hello. this is seperate route")
   });
 
+router.use('/',(req, res, next) =>{
+    for (let i = 1; i <= 3; i++) {
+          console.log("Outer loop i =", i);
+        
+          for (let j = 1; j <= 2; j++) {
+            console.log("  Inner loop j =", j);
+          }
+        }
+    res.send("hello. this is seperate route")
+  });
+
 module.exports.router = router;
